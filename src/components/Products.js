51c0a957d47f73d4f685a25206b5ab8380dc32e5
ntnang <ntnang91@gmail.com>
2021-12-05 16:08:14 +0700

    use DataScroller instead of DataView

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { DataView, DataViewLayoutOptions } from "primereact/dataview";
+import { DataScroller } from "primereact/datascroller";
 import ProductService from "../service/ProductService";
 import "../scss/products.scss";
 
 const Products = () => {
   const [products, setProducts] = useState();
-  const [layout, setLayout] = useState("grid");
 
   const productService = new ProductService();
 
@@ -15,90 +14,35 @@ const Products = () => {
     });
   }, []);
 
-  const renderHeader = () => {
-    let onOptionChange = (e) => {
-      setLayout(e.value);
-    };
-
+  const itemTemplate = (product) => {
     return (
-      <div style={{ textAlign: "left" }}>
-        <DataViewLayoutOptions layout={layout} onChange={onOptionChange} />
-      </div>
-    );
-  };
-
-  const header = renderHeader();
-
-  const itemTemplate = (product, layout) => {
-    if (!product) {
-      return;
-    }
-
-    if (layout === "list") return renderListItem(product);
-    else if (layout === "grid") return renderGridItem(product);
-  };
-
-  const renderListItem = (product) => {
-    return (
-      <div className="col-12">
-        <div className="product-list-item">
-          <img
-            src={`${product.thumbnailUrl}`}
-            onError={(e) =>
-              (e.target.src =
-                "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-            }
-            alt={product.name}
-          />
-          <div className="product-list-detail">
-            <div className="product-name">{product.name}</div>
+      <div className="product-item card">
+        <div className="product-item-top"></div>
+        <div className="product-item-content">
+          <div className="flex justify-content-center align-items-center">
+            <img
+              src={product.thumbnailUrl}
+              onError={(e) =>
+                (e.target.src =
+                  "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+              }
+              alt={product.name}
+            />
           </div>
-          <div className="product-list-action">
-            {/* <span className="product-price">${product.price}</span> */}
-          </div>
+          <div className="product-name">{product.name}</div>
         </div>
-      </div>
-    );
-  };
-
-  const renderGridItem = (product) => {
-    return (
-      <div className="col-4">
-        <div className="product-grid-item card">
-          <div className="product-grid-item-top"></div>
-          <div className="product-grid-item-content">
-            <div className="flex justify-content-center align-items-center">
-              <img
-                src={product.thumbnailUrl}
-                onError={(e) =>
-                  (e.target.src =
-                    "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-                }
-                alt={product.name}
-              />
-            </div>
-            <div className="product-name">{product.name}</div>
-          </div>
-          <div className="product-grid-item-bottom">
-            {/* <span className="product-price">${product.price}</span> */}
-          </div>
+        <div className="product-item-bottom">
+          {/* <span className="product-price">${product.price}</span> */}
         </div>
       </div>
     );
   };
 
   return (
-    <div className="grid">
-      <div className="col-12">
-        <div className="card">
-          <h5>Tracked Products</h5>
-          <DataView
-            value={products}
-            layout={layout}
-            header={header}
-            itemTemplate={itemTemplate}
-          />
-        </div>
+    <div className="col-12">
+      <div className="card">
+        <h5>Tracked Products</h5>
+        <DataScroller value={products} itemTemplate={itemTemplate} rows={3} />
       </div>
     </div>
   );
