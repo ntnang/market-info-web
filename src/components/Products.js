790fb5c5722216303c4c5c72dd3d822c1cfd9d21
ntnang <ntnang91@gmail.com>
2021-12-12 11:22:30 +0700

    show seller logos and format prices

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { DataScroller } from "primereact/datascroller";
 import ProductService from "../service/ProductService";
-import ProductOrigins from "../constants/ProductOrigins";
 import "../scss/products.scss";
 
 const Products = () => {
   const [products, setProducts] = useState();
 
   const productService = new ProductService();
-  const productOrigins = new ProductOrigins();
 
   useEffect(() => {
     productService.getAllProducts().then((products) => {
@@ -17,11 +15,15 @@ const Products = () => {
   }, []);
 
   const itemTemplate = (product) => {
+    const firstSeller = Array.from(product.sellers, ([_, value]) => value)[0];
+    const latestPriceOfFirstSeller = firstSeller.priceHistories[0];
     return (
       <div className="product-item card">
         <div className="product-item-top">
           <img
             src={`https://${product.origin}/favicon.ico`}
+            title={product.origin}
+            alt={product.origin}
             onError={(e) =>
               (e.target.src =
                 "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
@@ -40,15 +42,26 @@ const Products = () => {
               alt={product.name}
             />
           </div>
-          <div className="product-name">{product.name}</div>
+          <div className="product-name" title={product.name}>
+            {product.name}
+          </div>
         </div>
         <div className="product-item-bottom">
-          <span className="product-price">
-            {
-              Array.from(product.sellers, ([_, value]) => value)[0]
-                .priceHistories[0].price
-            }
-          </span>
+          <div className="product-seller flex">
+            <img
+              src={firstSeller.logoUrl}
+              title={firstSeller.name}
+              alt={firstSeller.name}
+              width={32}
+              height={32}
+            />
+            <span className="product-price">
+              {new Intl.NumberFormat("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              }).format(latestPriceOfFirstSeller.price)}
+            </span>
+          </div>
         </div>
       </div>
     );
