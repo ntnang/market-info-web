772de51cfe8414573a2f9e27bea4a348ecfb848a
ntnang <ntnang91@gmail.com>
2022-01-17 15:20:22 +0700

    separate code for product price chart into a new component for reusing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Chart } from "primereact/chart";
-import { Dropdown } from "primereact/dropdown";
+import ProductPriceChart from "../components/ProductPriceChart";
 import ProductService from "../service/ProductService";
-import ChartDatasetBuilder from "../util/ChartDatasetBuilder";
-import TimeSpan from "../constants/TimeSpan";
 
 const Dashboard = (props) => {
   const [latestProduct, setLatestProduct] = useState({
@@ -14,91 +11,20 @@ const Dashboard = (props) => {
     lastTrackedDate: null,
   });
 
-  const [productPriceHistoryChartModel, setProductPriceHistoryChartModel] =
-    useState({
-      name: "",
-      history: {
-        labels: [],
-        datasets: [],
-      },
-    });
-
   const productService = new ProductService();
-  const chartDatasetBuilder = new ChartDatasetBuilder();
-  const timeSpan = new TimeSpan();
-
-  const timeSpans = timeSpan.VALUES.map((timeSpanValue) => {
-    return { label: timeSpanValue.label, value: timeSpanValue.id };
-  });
-  const timeSpanMap = new Map(
-    timeSpan.VALUES.map((timeSpanValue) => [
-      timeSpanValue.id,
-      timeSpanValue.value,
-    ])
-  );
-
-  const [selectedTimespanId, setSelectedTimespanId] = useState(
-    timeSpan.LAST_7_DAYS.id
-  );
 
   useEffect(() => {
-    const selectedTimespan = timeSpanMap.get(selectedTimespanId);
-    const displayedPointOfTimeLabels = selectedTimespan
-      .pointsOfTime()
-      .map((date) =>
-        new Intl.DateTimeFormat(
-          "vi-VN",
-          selectedTimespan.displayTimeFormat
-        ).format(date)
-      );
     productService.findLastTrackedProduct().then((product) => {
       setLatestProduct(product);
-      setProductPriceHistoryChartModel({
-        name: product.name,
-        history: {
-          labels: displayedPointOfTimeLabels,
-          datasets: chartDatasetBuilder.buildChartDataSets(
-            product,
-            selectedTimespan
-          ),
-        },
-      });
     });
   }, [props.lastChangeDateTime]);
-
-  useEffect(() => {
-    const selectedTimespan = timeSpanMap.get(selectedTimespanId);
-    const displayedPointOfTimeLabels = selectedTimespan
-      .pointsOfTime()
-      .map((date) =>
-        new Intl.DateTimeFormat(
-          "vi-VN",
-          selectedTimespan.displayTimeFormat
-        ).format(date)
-      );
-    setProductPriceHistoryChartModel({
-      name: latestProduct.name,
-      history: {
-        labels: displayedPointOfTimeLabels,
-        datasets: chartDatasetBuilder.buildChartDataSets(
-          latestProduct,
-          selectedTimespan
-        ),
-      },
-    });
-  }, [selectedTimespanId]);
 
   return (
     <div className="grid">
       <div className="col-12 xl:col-12">
         <div className="card">
           <h5>{latestProduct.name}</h5>
-          <Dropdown
-            options={timeSpans}
-            value={selectedTimespanId}
-            onChange={(e) => setSelectedTimespanId(e.value)}
-          />
-          <Chart type="line" data={productPriceHistoryChartModel.history} />
+          <ProductPriceChart product={latestProduct} />
         </div>
       </div>
     </div>
--- src/components/ProductPriceChart.js
+++ src/components/ProductPriceChart.js
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
+import { Dropdown } from "primereact/dropdown";
+import ChartDatasetBuilder from "../util/ChartDatasetBuilder";
+import TimeSpan from "../constants/TimeSpan";
 
-const ProductPriceChart = (props) => {};
+const ProductPriceChart = (props) => {
+  const [productPriceHistoryChartModel, setProductPriceHistoryChartModel] =
+    useState({
+      name: "",
+      history: {
+        labels: [],
+        datasets: [],
+      },
+    });
+
+  const chartDatasetBuilder = new ChartDatasetBuilder();
+  const timeSpan = new TimeSpan();
+
+  const [selectedTimespanId, setSelectedTimespanId] = useState(
+    timeSpan.LAST_7_DAYS.id
+  );
+
+  const timeSpans = timeSpan.VALUES.map((timeSpanValue) => {
+    return { label: timeSpanValue.label, value: timeSpanValue.id };
+  });
+  const timeSpanMap = new Map(
+    timeSpan.VALUES.map((timeSpanValue) => [
+      timeSpanValue.id,
+      timeSpanValue.value,
+    ])
+  );
+
+  useEffect(() => {
+    const selectedTimespan = timeSpanMap.get(selectedTimespanId);
+    const displayedPointOfTimeLabels = selectedTimespan
+      .pointsOfTime()
+      .map((date) =>
+        new Intl.DateTimeFormat(
+          "vi-VN",
+          selectedTimespan.displayTimeFormat
+        ).format(date)
+      );
+    setProductPriceHistoryChartModel({
+      name: props.product.name,
+      history: {
+        labels: displayedPointOfTimeLabels,
+        datasets: chartDatasetBuilder.buildChartDataSets(
+          props.product,
+          selectedTimespan
+        ),
+      },
+    });
+  }, [props.product, selectedTimespanId]);
+
+  //   useEffect(() => {
+  //     const selectedTimespan = timeSpanMap.get(selectedTimespanId);
+  //     const displayedPointOfTimeLabels = selectedTimespan
+  //       .pointsOfTime()
+  //       .map((date) =>
+  //         new Intl.DateTimeFormat(
+  //           "vi-VN",
+  //           selectedTimespan.displayTimeFormat
+  //         ).format(date)
+  //       );
+  //     setProductPriceHistoryChartModel({
+  //       name: props.product.name,
+  //       history: {
+  //         labels: displayedPointOfTimeLabels,
+  //         datasets: chartDatasetBuilder.buildChartDataSets(
+  //           props.product,
+  //           selectedTimespan
+  //         ),
+  //       },
+  //     });
+  //   }, [selectedTimespanId]);
+
+  return (
+    <React.Fragment>
+      <Dropdown
+        options={timeSpans}
+        value={selectedTimespanId}
+        onChange={(e) => setSelectedTimespanId(e.value)}
+      />
+      <Chart type="line" data={productPriceHistoryChartModel.history} />
+    </React.Fragment>
+  );
+};
 
 export default ProductPriceChart;
