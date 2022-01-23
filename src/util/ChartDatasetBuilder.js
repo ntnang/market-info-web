3e565d2a6db8143aadcc6ea92bedbed7b5747efb
ntnang <ntnang91@gmail.com>
2022-01-23 21:17:17 +0700

    add colors for chart datasets

--- src/util/ChartDatasetBuilder.js
+++ src/util/ChartDatasetBuilder.js
@@ -1,21 +1,47 @@
 class ChartDatasetBuilder {
+  chartDatasetHexColors = [
+    "#ff9900",
+    "#ffcc00",
+    "#ffcc66",
+    "#ffcc99",
+    "#ffcccc",
+    "#ffcccc",
+    "#ffccff",
+    "#cc99ff",
+    "#cc99ff",
+    "#6666ff",
+    "#cccc00",
+    "#ccff33",
+    "#ccff66",
+    "#ccff99",
+    "#ccffcc",
+    "#ccffff",
+    "#66ccff",
+    "#3399ff",
+  ];
+
   buildChartDataSets(product, timeSpan) {
     const datasets = [];
-    for (let seller of product.sellers.values()) {
-      const dataset = this.buildChartDataset(seller, timeSpan);
+    Array.from(product.sellers.values()).forEach((seller, index) => {
+      const dataset = this.buildChartDataset(
+        seller,
+        timeSpan,
+        this.chartDatasetHexColors[index % this.chartDatasetHexColors.length]
+      );
       if (dataset.data && dataset.data.some((data) => data !== null)) {
         datasets.push(dataset);
       }
-    }
+    });
     return datasets;
   }
 
-  buildChartDataset(seller, timeSpan) {
+  buildChartDataset(seller, timeSpan, color) {
     const dataset = {};
     dataset.data = this.generateChartData(seller.priceHistories, timeSpan);
     dataset.label = seller.name;
     dataset.tension = 0.4;
     dataset.fill = false;
+    dataset.borderColor = color;
     return dataset;
   }
 
